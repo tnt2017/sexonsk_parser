4bc048a64cca53e662ba41fab2944d2e28ff3f14
tnt2017 <tnt2011@bk.ru>
2019-02-03 13:11:31 +0700

    Add files via upload

---
+++ sexonsk_parser/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button10 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(589, 223);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 250);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(589, 191);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(607, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 447);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(762, 175);
+            this.textBox3.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(688, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Parse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(607, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Parse pics";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(608, 280);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox4.Size = new System.Drawing.Size(166, 97);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.WordWrap = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(608, 161);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(166, 83);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(688, 50);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Parse * pics";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 1);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(762, 14);
+            this.progressBar1.TabIndex = 9;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(607, 79);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Parse skills";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(688, 79);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Parse * skills";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(607, 108);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(156, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Parse comms";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(607, 137);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(156, 23);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "Parse * comms";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(608, 251);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(94, 23);
+            this.button9.TabIndex = 14;
+            this.button9.Text = "download pic";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(608, 383);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(166, 58);
+            this.dataGridView2.TabIndex = 15;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(699, 250);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 16;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 634);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ sexonsk_parser/Form1.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Net;
+using System.IO;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection con;
+        string ConnectionString="server=127.0.0.1;user=root;database=girls;password=++++++;charset=utf8; Allow Zero Datetime=true;";
+
+        public string gethtml(string url)
+        {
+            HttpWebRequest proxy_request = (HttpWebRequest)WebRequest.Create(url);
+            proxy_request.Method = "GET";
+            proxy_request.ContentType = "application/x-www-form-urlencoded";
+            proxy_request.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.89 Safari/532.5";
+            proxy_request.KeepAlive = true;
+            HttpWebResponse resp = proxy_request.GetResponse() as HttpWebResponse;
+            string html = "";
+            using (StreamReader sr = new StreamReader(resp.GetResponseStream(), Encoding.GetEncoding("utf-8")))
+                html = sr.ReadToEnd();
+            html = html.Trim();
+            return html;
+        }
+
+
+        public void SqlQuery(string sql, string message)
+        {
+            using (MySqlConnection con = new MySqlConnection(ConnectionString))
+            {
+                try
+                {
+                    MySqlCommand cmd = new MySqlCommand(sql, con);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    //MessageBox.Show(message);
+                }
+
+                catch (Exception ex)
+                {
+                   // MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        
+        public DataTable Get_DataTable(string queryString)
+        {
+            DataTable dt = new DataTable();
+            MySqlCommand com = new MySqlCommand(queryString, con);
+
+            try
+            {
+                using (MySqlDataReader dr = com.ExecuteReader())
+                {
+                    if (dr.HasRows)
+                    {
+                        dt.Load(dr);
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return dt;
+        }
+
+        public DataTable GetGirls(string id)
+        {
+            string q = @"SELECT pid FROM tab_girls";
+            return Get_DataTable(q);
+        }
+
+        public DataTable GetPhotos(string id)
+        {
+            string q = @"SELECT big_photo, small_photo FROM photos";
+            return Get_DataTable(q);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string clr(string s)
+        {
+            s = s.Substring(s.IndexOf("pink\">")+6, s.Length - s.IndexOf("pink\">")-6);
+            s = s.Substring(0, s.IndexOf("<"));
+            return s;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            // читаем файл в строку
+            string fileText = System.IO.File.ReadAllText(filename);
+            textBox1.Text = fileText;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con = new MySqlConnection();
+            con.ConnectionString = ConnectionString;
+            MySqlConnectionStringBuilder mysqlCSB = new MySqlConnectionStringBuilder();
+            mysqlCSB.ConnectionString = ConnectionString;
+            con.Open();
+           // SqlQuery("INSERT into tab_girls VALUES(NULL,'1','2','3','','','','','','','','');","OK");
+            dataGridView1.DataSource = GetGirls("");
+
+            dataGridView2.DataSource = GetPhotos("");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+
+            for (; ; )
+            {
+                s = s.Substring(s.IndexOf("\"girl_info\"") + 1, s.Length - s.IndexOf("\"girl_info\"") - 1);
+
+                int ipos = s.IndexOf("\"girl_info\"");
+                if (ipos < 0)
+                    break;
+
+                string s1 = s.Substring(0, ipos); //
+
+                textBox2.Text = s1;
+
+                string pid = s1.Substring(s1.IndexOf("profile")+8, 7);
+                pid = pid.Substring(0, pid.IndexOf("\""));
+                //MessageBox.Show(id);
+                
+                string phone = s1.Substring(s1.IndexOf("girl_phone") + 16, 15);
+                string description = "";
+
+                if (s1.IndexOf("girl_discription") > 0)
+                {
+                    description = s1.Substring(s1.IndexOf("girl_discription") + 36, s1.Length - s1.IndexOf("girl_discription") - 36);
+
+                    if (description.IndexOf("<br>") > 0)
+                        description = description.Substring(0, description.IndexOf("<br>"));
+                }
+
+                // MessageBox.Show(description);
+                string photo = s1.Substring(s1.IndexOf("_files/prost") + 7, 46);
+                photo = photo.Substring(0, photo.IndexOf("\""));
+                //MessageBox.Show(photo);
+
+                string nameraion = s1.Substring(s1.IndexOf("girl_name fll") + 16, 150);
+                string name = nameraion.Substring(nameraion.IndexOf(">") + 1, nameraion.Length - nameraion.IndexOf(">") - 1);
+                name = name.Substring(0, name.IndexOf("<"));
+                string raion = nameraion.Substring(nameraion.IndexOf("<br>") + 4, nameraion.Length - nameraion.IndexOf("<br>") - 4);
+                raion = raion.Substring(0, raion.IndexOf("</div>"));
+
+                string vozrast = s1.Substring(s1.IndexOf("Возраст"), 40); vozrast = clr(vozrast);
+                string rost = s1.Substring(s1.IndexOf("Рост"), 40); rost = clr(rost);
+                string ves = s1.Substring(s1.IndexOf("Вес"), 50); ves = clr(ves);
+                string grud = s1.Substring(s1.IndexOf("Грудь"), 50); grud = clr(grud);
+                string chas = s1.Substring(s1.IndexOf("1 час"), 50); chas = clr(chas);
+                string dvachasa = s1.Substring(s1.IndexOf("2 часа"), 50); dvachasa = clr(dvachasa);
+                string noch = s1.Substring(s1.IndexOf("Ночь"), 50); noch = clr(noch);
+                string viezd = s1.Substring(s1.IndexOf("Выезд"), 50); viezd = clr(viezd).Trim();
+
+                textBox3.Text += "\r\n" + name + " " + raion + " " + phone + " " + photo + "\r\n" + vozrast + " " + rost + " " + ves + " " + grud + " " + chas + " " + dvachasa + " " + noch + " " + viezd;
+                textBox3.Text += description;
+                SqlQuery("INSERT into tab_girls VALUES(NULL,'" + pid + "'," + "NOW()" + ",'" + name + "','" + raion + "','" + phone + "','" + vozrast + "','" + rost + "','" + ves + "','" + grud + "','" + chas + "','" + dvachasa + "','" + noch + "','" + photo + "','" + description + "','" + viezd + "')", "OK");
+            }
+        }
+
+
+        public void get_photos(string pid)
+        {
+            string s = gethtml("https://sexonsk.online/prostitutka/" + pid + "/");
+
+            for (;;)
+            {
+                s = s.Substring(s.IndexOf("<a href='/photos/prostitutka") + 17, s.Length - s.IndexOf("<a href='/photos/prostitutka") - 17);
+
+                int ipos = s.IndexOf("<a href='/photos/prostitutka");
+                if (ipos < 0)
+                    break;
+
+                string s1 = s.Substring(0, 260);
+
+                string bigphoto=s1.Substring(0,s1.IndexOf("'"));
+                string smallphoto = s1.Substring(s1.IndexOf("'/")+1, s1.Length - s1.IndexOf("'/")-1);
+                smallphoto = smallphoto.Substring(0, smallphoto.IndexOf("'"));
+                
+                SqlQuery("INSERT into photos VALUES(NULL, NOW(), '" + pid + "','" + bigphoto + "','" + smallphoto + "')", "OK");
+            }   
+        }
+
+        public void get_skills(string pid)
+        {
+            string s = gethtml("https://sexonsk.online/prostitutka/" + pid + "/");
+            Dictionary<string, int> map = new Dictionary<string, int>();
+
+            map.Add("Классический секс", 1);
+            map.Add("Оральный секс", 1);
+            map.Add("Анальный секс", 1); //1
+            map.Add("Принимаю кунилингус", 1);//4
+            map.Add("Минет без презерватива", 1);//3
+            map.Add("Групповой секс", 1);
+            map.Add("Эротический массаж", 1);
+            map.Add("Стриптиз", 1);//8
+            map.Add("Секс с семейными парами", 1); //2
+            map.Add("Лесбийский секс", 1);//7
+            map.Add("Делаю кунилингус", 1);
+            map.Add("Ролевые игры", 1);//5
+            map.Add("Страпон", 1);//5
+            map.Add("Игрушки", 1);
+            map.Add("Золотой дождь", 1);
+            map.Add("Эскорт", 1);
+
+            for (;;)
+            {
+                s = s.Substring(s.IndexOf("strike") + 8, s.Length - s.IndexOf("strike") - 8);
+
+                int ipos = s.IndexOf("strike");
+                if (ipos < 0)
+                    break;
+
+                string s1 = s.Substring(0, 100);
+                s1 = s1.Substring(0, s1.IndexOf("<"));
+                map[s1] = 0;
+            }
+
+            string outs = "";
+            string sql_req="";
+            foreach (var pair in map)
+            {
+                outs += pair.Key + ":" + pair.Value + "\r\n";
+                sql_req += ", '" + pair.Value + "'";
+            }
+            
+            string query="INSERT into skills VALUES(NULL, '" + pid + "','" + outs + "'" + sql_req + ")";
+            //MessageBox.Show(query);
+            SqlQuery(query, "OK");
+        }
+
+        public void get_comments(string pid)
+        {
+            string s = gethtml("https://sexonsk.online/prostitutka/" + pid + "/");
+
+
+            for (; ; )
+            {
+                s = s.Substring(s.IndexOf("word-wrap:break-word;") + 23, s.Length - s.IndexOf("word-wrap:break-word;") - 23);
+
+                int ipos = s.IndexOf("word-wrap:break-word;");
+                if (ipos < 0)
+                    break;
+
+                string s1 = s.Substring(0, 1300);
+
+                string comment = s1.Substring(0, s1.IndexOf("<"));
+                string name = s1.Substring(s1.IndexOf("Написал(") + 14, 100);
+                string date = name.Substring(name.IndexOf("</b>") + 6, 30);
+                name = name.Substring(0, name.IndexOf("</b>"));
+                name = name.Trim();
+                date = date.Substring(0, date.IndexOf(".<"));
+
+                //MessageBox.Show(comment);
+                //MessageBox.Show(name);
+                //MessageBox.Show(date);
+
+                string query = "INSERT into comments VALUES(NULL, '" + pid + "','" + comment + "','" + name + "','" + date + "')";
+                // MessageBox.Show(query);
+                SqlQuery(query, "OK");
+            }
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string pid = dataGridView1.CurrentCell.Value.ToString();
+            get_photos(pid);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string s = dataGridView1.CurrentCell.Value.ToString();
+            MessageBox.Show(s);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = dataGridView1.Rows.Count;
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                string pid=dataGridView1.Rows[i].Cells[0].Value.ToString();
+                get_photos(pid);
+                progressBar1.Value++;
+                Application.DoEvents();
+                this.Text = i.ToString();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string pid = "160194";
+            get_skills(pid);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = dataGridView1.Rows.Count;
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                string pid = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                get_skills(pid);
+                progressBar1.Value++;
+                Application.DoEvents();
+                this.Text = i.ToString();
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string pid = "311034";
+            get_comments(pid);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = dataGridView1.Rows.Count;
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                string pid = dataGridView1.Rows[i].Cells[0].Value.ToString();
+                get_comments(pid);
+                progressBar1.Value++;
+                Application.DoEvents();
+                this.Text = i.ToString();
+            }
+        }
+
+        private void DownloadPhoto(string fn)
+        {
+            fn = fn.Substring(0, fn.IndexOf("?"));
+            WebClient wc = new WebClient();
+            string url = "https://sexonsk.online/photos/140_210/" + fn;
+            string save_path = Path.GetDirectoryName(Application.ExecutablePath) + "\\";
+
+            //MessageBox.Show(save_path);
+            string name = fn;
+            wc.DownloadFile(url, save_path + name);
+        }
+
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            DownloadPhoto("prostitutka_318451_5c4ac95bbe340_r.jpg");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = dataGridView2.Rows.Count;
+            for (int i = 0; i < dataGridView2.Rows.Count - 1; i++)
+            {
+                string pid = dataGridView2.Rows[i].Cells[0].Value.ToString();
+
+                DownloadPhoto(pid);
+
+                progressBar1.Value++;
+                Application.DoEvents();
+                this.Text = i.ToString();
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
